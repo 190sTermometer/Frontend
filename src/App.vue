1a7e06db3dbdbc343a2bb8ea97776fecd1ed9170
abbsimris <simon.risberg@abbindustrigymnasium.se>
2019-10-08 12:31:01 +0200

    ddd

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> <div>HejSan</div>
     <router-view />
   </div>
   <div></div>
